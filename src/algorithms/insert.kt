056d5e9bd3d3bf055b6decdba9ba48026ad48ac1
Muhammad Magdi <Muhammad.Magdi1024@gmail.com>
2019-11-03 16:40:44 +0200

    Initial Commit with `insert` function

---
+++ src/algorithms/insert.kt
@@ -0,0 +1,23 @@
+package algorithms
+
+/**
+ * Inserts the `value` in its correct position in the `arr`, so that `arr` becomes ascendingly sorted.
+ *
+ *  Assumptions:
+ *  - `arr` is an array of size at least `sortedSize`+1.
+ *  - `arr` is an array whose values in indices [0...sortedSize-1] are already sorted ascendingly.
+ *
+ *  Time Complexity:
+ *  - O(sortedSize)
+ *
+ *  Memory Complexity:
+ *  - O(1)
+ */
+fun insert(arr: IntArray, sortedSize: Int, value: Int) {
+    var i = sortedSize-1
+    while (i >= 0 && arr[i] > value) {
+        arr[i+1] = arr[i]
+        --i
+    }
+    arr[i+1] = value
+}
---
+++ src/main.kt
@@ -0,0 +1,7 @@
+import algorithms.*
+
+fun main() {
+    val arr = intArrayOf(1, 3, 5, 7, 6, 10, -1, 2)
+    insert(arr, 4, arr[4])
+    arr.forEach { println(it) }
+}
