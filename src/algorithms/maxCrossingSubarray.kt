290fa3450354eaa4d9b73b0af7160ba20eb00acc
Muhammad Magdi <Muhammad.Magdi1024@gmail.com>
2019-11-11 03:05:54 +0200

    Add the "maxCrossingSubArray" function

---
+++ src/algorithms/maxCrossingSubarray.kt
@@ -0,0 +1,20 @@
+package algorithms
+
+/**
+ * - Finds the maximum subArray sum in the range [low, high] that crosses 'mid'
+ * - Returns a Triple that contains <Thr starting index, The ending index, The maximum sum>
+ *
+ *  Assumptions:
+ *  low <= mid < high
+ *
+ *  Time Complexity:
+ *  - O(|end - start|)
+ *
+ *  Memory Complexity:
+ *  - O(1)
+ */
+fun maxCrossingSubArray(arr: IntArray, low: Int, mid: Int, high: Int) : Triple<Int, Int, Int>{
+  val bestSuffix = maxPrefixSum(arr, mid, low)
+  val bestPrefix = maxPrefixSum(arr, mid + 1, high)
+  return Triple(bestSuffix.first, bestPrefix.first, bestPrefix.second + bestSuffix.second)
+}
--- src/main.kt
+++ src/main.kt
@@ -1,7 +1,7 @@
 import algorithms.*
 
 fun main() {
-  val arr = intArrayOf(1, 3, 5, 7, -1, 2, 6, 10)
+  val arr = intArrayOf(1, 3, 5, -7, -1, 2, 6, 10)
 //  insert(arr, 4, arr[4])
 //  insertionSort(arr)
 //  val minIndex = selectMin(arr)
@@ -11,5 +11,6 @@ fun main() {
 //  mergeSort(arr)
 //  bubbleSort(arr)
 //  arr.forEach { println(it) }
-  println(maxPrefixSum(arr, 0, 5))
+//  println(maxPrefixSum(arr, 0, 5))
+  println(maxCrossingSubArray(arr, 3, 4, 5))
 }
