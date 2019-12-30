ba378d7dbba9b85140a2c8757a9baa238edd6855
Muhammad Magdi <Muhammad.Magdi1024@gmail.com>
2019-12-30 08:04:05 +0200

    Add the implementation for the `partition` procedure

---
+++ src/algorithms/naive/partition.kt
@@ -0,0 +1,23 @@
+package algorithms.naive
+
+/**
+ * Places the last value in the array into its correct position, such that all the preceding values
+ * are smaller that it and all the succeeding values are greater that it.
+ *
+ *  Time Complexity:
+ *  - O(A.size)
+ *
+ *  Memory Complexity:
+ *  - O(1)
+ */
+fun partition(A: IntArray, startIndex: Int, endIndex: Int): Int {
+  var sureSmaller = startIndex - 1
+  for (i in startIndex until endIndex - 1) {
+    if (A[i] <= A[endIndex - 1]) {
+      ++sureSmaller
+      A[sureSmaller] = A[i].also { A[i] = A[sureSmaller] }
+    }
+  }
+  A[sureSmaller + 1] = A[endIndex - 1].also { A[endIndex - 1] = A[sureSmaller + 1] }
+  return sureSmaller + 1
+}
